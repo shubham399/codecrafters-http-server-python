2d505c9175eb6ffd0c34e75dd1ff3e210b1e089e
Shubham Kumar <hello@shubhkumar.in>
2023-09-22 23:34:19 +0530

    clean

--- app/main.py
+++ app/main.py
@@ -1,31 +1,80 @@
-# Uncomment this to pass the first stage
 import socket
 
 
-# parse received HTTP request
-def parse(data):
-    lines = data.split(b"\r\n")
-    # print(lines[0].split(b' '))
-    path = lines[0].split(b" ")[1]
-    if path == b"/":
-        return b"HTTP/1.1 200 OK\r\n\r\n"
-    else:
-        return b"HTTP/1.1 404 Not Found\r\n\r\n"
+class HTTPResponse:
+    def __init__(self, status_code: int, body: bytes = None, headers: dict = None):
+        self.status_code = status_code
+        self.body = body
+        self.headers = headers or {}
+
+    @property
+    def status_line(self):
+        return f"HTTP/1.1 {self.status_code}"
+
+    @property
+    def headers_section(self):
+        return "\r\n".join(
+            f"{k}: {v}"
+            for k, v in {
+                "Content-Length": len(self.body) if self.body else 0,
+                **self.headers,
+            }.items()
+        )
+
+    def __bytes__(self):
+        return (
+            self.status_line.encode("utf-8")
+            + b"\r\n"
+            + self.headers_section.encode("utf-8")
+            + b"\r\n\r\n"
+            + (self.body or b"")
+        )
+
+
+class HTTPRequest:
+    raw_contents: bytes
+
+    def __init__(self, raw_contents: bytes):
+        self.raw_contents = raw_contents
+
+    @property
+    def method(self):
+        return self.status_line().split(b" ")[0].decode("utf-8")
+
+    @property
+    def path(self):
+        return self.status_line().split(b" ")[1].decode("utf-8")
+
+    @property
+    def protocol(self):
+        return self.status_line().split(b" ")[2].decode("utf-8")
+
+    def __repr__(self):
+        return f"<HTTPRequest {self.method} {self.path}>"
+
+    def status_line(self):
+        return self.raw_contents.split(b"\r\n")[0]
+
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, address = server_socket.accept()  # wait for client
-    
-    data = conn.recv(1024)
-    # parse the data
-    res = parse(data)
-    print(res)
-    conn.sendall(res)
+    conn, addr = server_socket.accept()  # wait for client
+    print("Client connected", addr)
+
+    request = HTTPRequest(conn.recv(1024))
+    print(request)
+
+    if request.path == "/":
+        conn.sendall(bytes(HTTPResponse(200)))
+    elif request.path.startswith("/echo"):
+        value = request.path.split("/echo/")[1]
+        response = HTTPResponse(
+            200, body=value.encode("utf-8"), headers={"Content-Type": "text/plain"}
+        )
+        conn.sendall(bytes(response))
+    else:
+        conn.sendall(bytes(HTTPResponse(404)))
+
 
 if __name__ == "__main__":
     main()
